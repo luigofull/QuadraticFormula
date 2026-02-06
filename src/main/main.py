e4ae64a5a5fa811cd9cf399e7511861752967b8c
luigofull <githubars441@gmail.com>
2026-02-06 16:19:55 +0200

    Small update

--- src/main/main.py
+++ src/main/main.py
@@ -1,37 +1,41 @@
+# lib imports
 from manim import *
 from MF_Tools import *
-from moves import *
+
+# project imports
+import moves as mv
+import exps as ex
 
 
 class Animation(Scene):
     def construct(self):
-        self.add(exp1)
+        self.add(ex.exp1)
         self.wait()
         
-        exp1to2(self)
-        exp2to3(self)
-        exp3to4(self)
-        exp4to5(self)
-        exp5to6(self)
-        exp6to7(self)
-        exp7to8(self)
-        exp8to9(self)
-        exp9to10(self)
-        exp10to11(self)
-        exp11to12(self)
-        exp12to13(self)
-        exp13to14(self)
-        exp14to15(self)
-        exp15to16(self)
-        exp16to17(self)
-        exp17to18(self)
-        exp18to19(self)
-        exp19to20(self)
-        exp20to21(self)
-        exp21to22(self)
+        mv.exp1to2(self)
+        mv.exp2to3(self)
+        mv.exp3to4(self)
+        mv.exp4to5(self)
+        mv.exp5to6(self)
+        mv.exp6to7(self)
+        mv.exp7to8(self)
+        mv.exp8to9(self)
+        mv.exp9to10(self)
+        mv.exp10to11(self)
+        mv.exp11to12(self)
+        mv.exp12to13(self)
+        mv.exp13to14(self)
+        mv.exp14to15(self)
+        mv.exp15to16(self)
+        mv.exp16to17(self)
+        mv.exp17to18(self)
+        mv.exp18to19(self)
+        mv.exp19to20(self)
+        mv.exp20to21(self)
+        mv.exp21to22(self)
         
         box = SurroundingRectangle(
-            exp22, 
+            ex.exp22, 
             color=YELLOW, 
             buff=0.2  
         )
